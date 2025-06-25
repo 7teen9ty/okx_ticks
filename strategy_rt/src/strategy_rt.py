1703688bdbbc64b21c6bbc0f028ed1e3d8301857
7teen9ty <azanow.artyom@yandex.ru>
2025-06-25 17:41:13 +0300

    add do dep

--- strategy_rt/src/strategy_rt.py
+++ strategy_rt/src/strategy_rt.py
@@ -20,6 +20,7 @@ SIZE_POSITION  = float(os.getenv("SIZE_POSITION", 0.03))
 STOP_LOSS_PIPS = float(os.getenv("STOP_LOSS_PIPS", 100))
 PG_TICKS_TABLE = os.getenv("PG_TICKS_TABLE", "raw_data.ticks")
 LEVERAGE       = int(os.getenv("LEVERAGE", 3))
+LIST_BUY_SIZE  = list(map(float, os.getenv("LIST_BUY", "0.03, 0.03, 0.06, 0.06").split(',')))
 
 API_KEY       = os.getenv("OKX_API_KEY", "")
 if not API_KEY:
@@ -63,26 +64,23 @@ async def tick_stream(max_retries=100, retry_delay=5):
 
 
 class Trade:
-    """
-    Управление одной позицией: вход и SL-мониторинг.
-    """
-    def __init__(self, symbol: str, entry_price: float, side: str, last_close: float):
+    def __init__(self, symbol: str, entry_price: float, side: str, last_close: float, on_close):
         self.symbol = f"{symbol}-SWAP"
         if not self._valid_name_symbol:
             raise ValueError(f'Ошибка в имени символа торговли {self.symbol} {symbol}')
         self.entry_price = entry_price
-        self.side = side   # 'long' или 'short'
+        self.side = side
         self.size = SIZE_POSITION
-        # первоначальный стоп-лосс
+        self.count = 1
         initial_sl = self._calc_stop_loss()
         self._set_leverage(LEVERAGE)
-        # корректируем SL на основании last_close
         if side == 'long':
             self.stop_loss = (last_close - STOP_LOSS_PIPS) if last_close < initial_sl else initial_sl
         else:
             self.stop_loss = (last_close + STOP_LOSS_PIPS) if last_close > initial_sl else initial_sl
 
         self.okx = TradeAPI(API_KEY, API_SECRET, API_PASSPHRASE, flag=API_FLAG)
+        self.on_close = on_close
         logging.info(f"[Trade] init {self.symbol=} {self.side=} entry={self.entry_price} sl={self.stop_loss}")
 
     def _valid_name_symbol(self):
@@ -90,11 +88,11 @@ class Trade:
         if len(split_symbol) == 3:
             return all(split_symbol[0] in ('BTC', 'ETH'), split_symbol[1] == 'USDT', split_symbol[2] == 'SWAP')
         return False
+
     def _calc_stop_loss(self) -> float:
         return self.entry_price - STOP_LOSS_PIPS if self.side == 'long' else self.entry_price + STOP_LOSS_PIPS
 
-    async def open_position(self):
-        """Исполнить рыночный ордер"""
+    async def open_position(self, size_dep=None):
         logging.info(f"[Trade] opening {self.side} for {self.symbol} @ {self.entry_price}")
         req = self.okx.place_order(
             instId=self.symbol,
@@ -102,25 +100,22 @@ class Trade:
             side=['sell', 'buy'][self.side == 'long'],
             ordType='market',
             sz=self.size,
-            posSide=self.side,
+            posSide=self.side if not size_dep else size_dep,
             tag="teststrategy"
-                    )
+        )
         logging.info(req)
-
-        return None
 
     def _set_leverage(self, leverage: int):
         account_api = AccountAPI(API_KEY, API_SECRET, API_PASSPHRASE, flag=API_FLAG)
-        account_api.set_leverage(        
-        instId=self.symbol,
-        lever=str(leverage),
-        mgnMode='isolated',
-        posSide=self.side
+        account_api.set_leverage(
+            instId=self.symbol,
+            lever=str(leverage),
+            mgnMode='isolated',
+            posSide=self.side
         )
         logging.info(f"[Trade] leverage set to {leverage} for {self.symbol} on {self.side} side")
 
-    async def monitor(self, on_close):
-        """Следим за тиками и вызываем колбек при SL срабатывании"""
+    async def monitor(self):
         time_start = time.time()
         closes = await load_4h_closes()
         async for symbol, px, _ in tick_stream():
@@ -130,11 +125,11 @@ class Trade:
                 symbol = f'{symbol}-SWAP'
             if symbol != self.symbol:
                 continue
-            utc_hour, utc_minute = [datetime.utcnow().hour,datetime.utcnow().minute]
-            if utc_hour in [00, 4, 8, 12, 16, 20, 24] and utc_minute == 0 and time_start - time.time() > 60:
-                "Обновление клоза за 4 часа"
+
+            utc_hour, utc_minute = datetime.utcnow().hour, datetime.utcnow().minute
+            if utc_minute == 0 and utc_hour % 4 == 0 and time.time() - time_start > 60:
                 logging.info('[INFO] Update close for 4 hours')
-                time.sleep(1)
+                await asyncio.sleep(1)
                 time_start = time.time()
                 closes = await load_4h_closes()
 
@@ -142,62 +137,66 @@ class Trade:
             stp_loss_now = [self.stop_loss < closes_symbol - STOP_LOSS_PIPS, self.stop_loss > closes_symbol + STOP_LOSS_PIPS][self.side == 'short']
             if stp_loss_now:
                 self.stop_loss = [closes_symbol - STOP_LOSS_PIPS, closes_symbol + STOP_LOSS_PIPS][self.side == 'short']
-            
+            if self.side == 'long' and px >= self.entry_price + (self.count * THRESHOLD) and self.count < 4:
+                await self.open_position(size_dep=LIST_BUY_SIZE[self.count])
+                self.count += 1
             if self.side == 'long' and px <= self.stop_loss:
                 logging.info(f"[Trade] SL hit LONG {self.symbol} @ {px}")
+
                 self.okx.place_order(
-                            instId=self.symbol,
-                            tdMode="isolated",
-                            side='sell',
-                            ordType='market',
-                            sz=self.size,
-                            posSide=self.side,
-                            tag="teststrategy"
-                                    )
-                break
+                    instId=self.symbol,
+                    tdMode="isolated",
+                    side='sell',
+                    ordType='market',
+                    sz=LIST_BUY_SIZE[self.count],
+                    posSide=self.side,
+                    tag="teststrategy"
+                )
+                self.count -= 1
+                if self.count <= 0:
+                    break
+
             if self.side == 'short' and px >= self.stop_loss:
                 logging.info(f"[Trade] SL hit SHORT {self.symbol} @ {px}")
                 self.okx.place_order(
-                            instId=self.symbol,
-                            tdMode="isolated",
-                            side='buy',
-                            ordType='market',
-                            sz=self.size,
-                            posSide=self.side,
-                            tag="teststrategy"
-                                    )
-                break
-        # вызываем колбек об окончании позиции
-        on_close(self.symbol)
+                    instId=self.symbol,
+                    tdMode="isolated",
+                    side='buy',
+                    ordType='market',
+                    sz=LIST_BUY_SIZE[self.count],
+                    posSide=self.side,
+                    tag="teststrategy"
+                )
+                self.count -= 1
+                if self.count <= 0:
+                    break
+
+        await self.on_close(self.symbol)
 
 async def strategy():
-    # загрузка последних 4h close
     closes = await load_4h_closes()
-    # активные позиции по символам
     active_positions = set()
-    # min/max за сессию
     mins = {s: float('inf') for s in OKX_INST_IDS}
     maxs = {s: float('-inf') for s in OKX_INST_IDS}
 
-    def on_trade_close(symbol):
-        """Колбек для удаления позиции из активных"""
+    async def on_trade_close(symbol):
         logging.info(f"[Strategy] position closed for {symbol}")
         active_positions.discard(symbol)
 
-    # слушаем тики
     logging.info('Tick starter')
     async for symbol, px, _ in tick_stream():
         if symbol not in OKX_INST_IDS:
             continue
         if symbol in active_positions:
-            continue  # только одна позиция одновременно
+            continue
         last_close = closes.get(symbol)
         if last_close is None:
             continue
+
         mins[symbol] = min(mins[symbol], px)
         maxs[symbol] = max(maxs[symbol], px)
         logging.info(f"[Pre trade] {mins[symbol]=} {maxs[symbol]=} {last_close=} {THRESHOLD}")
-        # проверка порога
+
         if maxs[symbol] - last_close >= THRESHOLD:
             side = 'long'
             entry = maxs[symbol]
@@ -206,14 +205,13 @@ async def strategy():
             entry = mins[symbol]
         else:
             continue
-        # запускаем позицию
-        trade = Trade(symbol, entry, side, last_close)
+
+        trade = Trade(symbol, entry, side, last_close, on_trade_close)
         await trade.open_position()
         active_positions.add(symbol)
         logging.info(active_positions)
 
-        asyncio.create_task(trade.monitor(on_trade_close))
-        # сбрасываем min/max
+        asyncio.create_task(trade.monitor())
         mins[symbol], maxs[symbol] = float('inf'), float('-inf')
 
 if __name__ == '__main__':
